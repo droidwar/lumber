705dc6f5c68613d3f623e5af79edc68adc712fcc
droidwar <76974855+droidwar@users.noreply.github.com>
2023-02-02 22:55:05 -0500

    Initial commit

---
+++ apps/kiipsy/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'kiipsy',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/kiipsy',
+};
